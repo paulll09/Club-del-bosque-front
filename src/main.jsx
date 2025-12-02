ce1a1f63e26633e4302f507d246f5d0690b9ca57
paulll09 <aquinopaul2002@gmail.com>
2025-12-02 17:07:10 -0300

    solucion ruta /admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
+import Admin from "./Admin.jsx"; // Panel admin
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Página principal (clientes) */}
+        <Route path="/" element={<App />} />
+
+        {/* Panel administrativo */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
