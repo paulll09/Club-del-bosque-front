bdcd5c11588bc301b28bd7ad42e09effc1aa00fb
paulll09 <aquinopaul2002@gmail.com>
2025-12-02 17:26:13 -0300

    solucion en vite.config (pantalla principal en blanco)

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
-/**
- * Configuración de Vite:
- * - Usa React
- * - Multipágina:
- *    /       -> index.html
- *    /admin  -> admin.html
- */
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        // Página principal (clientes)
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
 });
